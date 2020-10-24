d484f684d3f10ffa8feba0957908b17397482142
Isaac Kisakye <isaackisakye@gmail.com>
2020-10-24 10:41:18 +0300

    initial commit

---
+++ Desktop/Calc1/details.js
@@ -0,0 +1,32 @@
+//Operation:Addition,Division,Subtraction & Multiplication
+
+var firstnumber=1;
+var secondnumber=2;
+
+function addNumber(firstnumber, secondnumber){
+console.log(firstnumber+secondnumber);
+}
+
+function subtractnumber(firstnumber, secondnumber){
+    console.log(firstnumber-secondnumber);
+    }
+
+function dividenumber(firstnumber,secondnumber){
+    console.log(firstnumber/secondnumber);
+}
+
+function multiplynumber(firstnumber, secondnumber){
+    console.log(firstnumber*secondnumber);
+}
+
+
+addNumber(1,2);
+addNumber(40,40)
+
+subtractnumber(100,6);
+
+dividenumber(100,2);
+
+multiplynumber(8,8);
+
+
